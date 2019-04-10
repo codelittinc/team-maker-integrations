cd83331453863eed0e5d47ab10c7cfd1fe7c5a07
kaiomagalhaes <me@kaiomagalhaes.com>
2019-04-10 10:03:34 +0200

    create base gem structure

---
+++ lib/team/maker/integrations.rb
@@ -0,0 +1,9 @@
+require "team/maker/integrations/version"
+
+module Team
+  module Maker
+    module Integrations
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/team/maker/integrations/version.rb
@@ -0,0 +1,7 @@
+module Team
+  module Maker
+    module Integrations
+      VERSION = "0.1.0"
+    end
+  end
+end
