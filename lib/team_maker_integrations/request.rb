1c2bc15a4ffe5c2a0dc734c6cf8249bfff1bef52
kaiomagalhaes <me@kaiomagalhaes.com>
2019-04-10 10:43:32 +0200

    create request file

--- lib/team_maker_integrations/request.rb
+++ lib/team_maker_integrations/request.rb
@@ -1,1 +1,13 @@
+require 'net/http'
 
+module TeamMakerIntegrations
+	class Request
+		def self.get(url, use_ssl = true)
+			uri = URI.parse(url)
+			http = Net::HTTP.new(uri.host, uri.port)
+			http.use_ssl = use_ssl
+			request = Net::HTTP::Get.new(uri.request_uri)
+			puts http.request(request).body
+		end
+	end
+end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,6 @@
 require 'bundler/setup'
 require 'team_maker_integrations/purely_hr/index'
+require 'team_maker_integrations/request'
 
 RSpec.configure do |config|
   # Enable flags like --only-failures and --next-failure
